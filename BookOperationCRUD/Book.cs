f297108b53beb7307e2c98cf54884188c7a2417b
SnehalS26 <Snehalshirsath26@gmail.com>
2023-07-11 17:32:13 +0530

    Add project files.

---
+++ BookOperationCRUD/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOperationCRUD
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Book_name { get; set; }
+        public string Author_name { get; set; }
+
+        public int Book_price { get; set; }
+    }
+    public class BookCRUD
+    {
+        private List<Book> books;
+        public BookCRUD()
+        {
+            books = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        public void UpdateBook(Book book)
+        {
+            foreach(Book book1 in books)
+            {
+                if(book1.Id == book.Id)
+                {
+                    book1.Book_name = book.Book_name;
+                    book1.Author_name = book.Author_name;
+                    book1.Book_price = book.Book_price;
+                    break;
+                }
+            }
+        }
+        public void DeleteBook(int id)
+        {
+            foreach(Book book1 in books)
+            {
+                if( book1.Id == id)
+                {
+                    books.Remove(book1);
+                    break;
+                }
+            }
+        }
+
+        public List<Book> GetBooks()
+        {
+            return books;
+        }
+    }
+}
---
+++ BookOperationCRUD/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOperationCRUD
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCRUD bookCRUD = new BookCRUD();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1. Add Book Details:");
+                Console.WriteLine("2. Update Book Details");
+                Console.WriteLine("3. Delete Book Details");
+                Console.WriteLine("4. Display All Book Details");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Book book = new Book();
+                        Console.WriteLine("Enter Book Id:");
+                        book.Id= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Book Name:");
+                        book.Book_name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Book Author Name:");
+                        book.Author_name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Book Price:");
+                        book.Book_price = Convert.ToInt32(Console.ReadLine());
+                        bookCRUD.AddBook(book);
+                        break;
+                    case 2:
+                        Book book1 = new Book();
+                        Console.WriteLine("Enter modify Book Id:");
+                        book1.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Book Name:");
+                        book1.Book_name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Book Author Name:");
+                        book1.Author_name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Enter Book Price:");
+                        book1.Book_price = Convert.ToInt32(Console.ReadLine());
+                        bookCRUD.UpdateBook(book1);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Delete Book Id:");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        bookCRUD.DeleteBook(id);
+                        break;
+                    case 4:
+                        List<Book> list = bookCRUD.GetBooks();
+                        foreach(Book b in list)
+                        {
+                            Console.WriteLine($"{b.Id} --> {b.Book_name} --> {b.Author_name} --> {b.Book_price}");
+                        }
+                        break;
+                }
+                Console.WriteLine("Enter 1 Option");
+                option= Convert.ToInt32(Console.ReadLine());
+            }
+            while (option == 1);
+        }
+    }
+}
